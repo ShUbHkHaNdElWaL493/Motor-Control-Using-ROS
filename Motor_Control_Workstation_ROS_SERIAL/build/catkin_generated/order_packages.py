51bc360d13dbda7db6148af520db77abd67cbef6
Shubh Khandelwal <98233594+ShUbHkHaNdElWaL493@users.noreply.github.com>
2024-05-20 00:13:01 +0530

    Add files via upload

---
+++ Motor_Control_Workstation_ROS_SERIAL/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/shubh_khandelwal/Documents/ROS/Motor_Control_Workstation_ROS_SERIAL/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
---
+++ Motor_Control_Workstation_ROS_SERIAL/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/shubh_khandelwal/Documents/ROS/Motor_Control_Workstation_ROS_SERIAL/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/shubh_khandelwal/Documents/ROS/Motor_Control_Workstation_ROS_SERIAL/devel/lib:$LD_LIBRARY_PATH"
+export PWD='/home/shubh_khandelwal/Documents/ROS/Motor_Control_Workstation_ROS_SERIAL/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/shubh_khandelwal/Documents/ROS/Motor_Control_Workstation_ROS_SERIAL/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/shubh_khandelwal/Documents/ROS/Motor_Control_Workstation_ROS_SERIAL/src:$ROS_PACKAGE_PATH"
