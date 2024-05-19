e6434ef706c1cb8b4414707a1703ea31c6675fea
Shubh Khandelwal <98233594+ShUbHkHaNdElWaL493@users.noreply.github.com>
2024-05-20 00:00:45 +0530

    Add files via upload

---
+++ Motor_Control_Workstation_ROS_SERIAL/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/shubh_khandelwal/Documents/ROS/Motor_Control_Workstation_ROS_SERIAL/src
+# Build directory: /home/shubh_khandelwal/Documents/ROS/Motor_Control_Workstation_ROS_SERIAL/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("motor")
